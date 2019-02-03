c3afc2642acd97c37a2733953268fadf6f08fdbe
apena <andre.pena@zalando.de>
2019-02-03 22:58:01 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import * as React from "react";
+import ReactMde from "react-mde";
+import * as Showdown from "showdown";
+import "react-mde/lib/styles/css/react-mde-all.css";
+import "draft-js/dist/Draft.css";
+
+export default class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      value: "**Hello world!!!**"
+    };
+    this.converter = new Showdown.Converter({
+      tables: true,
+      simplifiedAutoLink: true,
+      strikethrough: true,
+      tasklists: true
+    });
+  }
+
+  handleValueChange = (value) => {
+    this.setState({ value });
+  };
+
+  render () {
+    return (
+      <div className="container">
+        <ReactMde
+          onChange={this.handleValueChange}
+          value={this.state.value}
+          generateMarkdownPreview={markdown =>
+            Promise.resolve(this.converter.makeHtml(markdown))
+          }
+        />
+      </div>
+    );
+  }
+}
