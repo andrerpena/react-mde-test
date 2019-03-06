e74fc871b735a2af28a2b899534d5fadbab80e74
andre <andrerpena@gmail.com>
2019-03-06 21:43:33 +0100

    Add readme

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
 import * as React from "react";
-import ReactMde from "react-mde";
+import ReactMde, {commands} from "react-mde";
 import * as Showdown from "showdown";
 import "react-mde/lib/styles/css/react-mde-all.css";
 
+const listCommands = [
+    {
+        commands: [
+            commands.orderedListCommand,
+            commands.unorderedListCommand,
+            commands.checkedListCommand
+        ]
+    }
+]
+
 export default class App extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      value: "**Hello world!!!**"
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "**Hello world!!!**"
+        };
+        this.converter = new Showdown.Converter({
+            tables: true,
+            simplifiedAutoLink: true,
+            strikethrough: true,
+            tasklists: true
+        });
+    }
+
+    handleValueChange = (value) => {
+        this.setState({value});
     };
-    this.converter = new Showdown.Converter({
-      tables: true,
-      simplifiedAutoLink: true,
-      strikethrough: true,
-      tasklists: true
-    });
-  }
 
-  handleValueChange = (value) => {
-    this.setState({ value });
-  };
-
-  render () {
-    return (
-      <div className="container">
-        <ReactMde
-          onChange={this.handleValueChange}
-          value={this.state.value}
-          generateMarkdownPreview={markdown =>
-            Promise.resolve(this.converter.makeHtml(markdown))
-          }
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="container">
+                <ReactMde
+                    onChange={this.handleValueChange}
+                    value={this.state.value}
+                    generateMarkdownPreview={markdown =>
+                        Promise.resolve(this.converter.makeHtml(markdown))}
+                    commands={listCommands}
+                />
+            </div>
+        );
+    }
 }
